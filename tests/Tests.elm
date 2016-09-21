26720d5e62ddb9a0eb8f7885175d3546a841103b
Ben Lopatin <ben@benlopatin.com>
2016-09-21 01:40:24 -0400

    Test character-configurable split functions
    splitWith, parseWith introduced in
    3ae19c052cafbe7091c0f59aab1e285baeca881d

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -13,23 +13,36 @@ all =
             \() ->
                 Expect.equal
                     [["id", "value"], ["1", "one"], ["2", "two"]] (Csv.split "id,value\n1,one\n2,two\n")
-        , test "split" <|
+        , test "split quotes" <|
             \() ->
                 Expect.equal
                       [["id", "value"], ["1", "one"], ["2", "two"]]
                                 (Csv.split
                                 "\"id\",\"value\"\n\"1\",\"one\"\n\"2\",\"two\"\n")
+        , test "split on semi-colon" <|
+            \() ->
+                Expect.equal
+                      [["id", "value"], ["1", "one"], ["2", "two"]]
+                                (Csv.splitWith ";"
+                                "\"id\";\"value\"\n\"1\";\"one\"\n\"2\";\"two\"\n")
         , test "parse" <|
             \() ->
                 Expect.equal
                       { headers = ["id", "value"], records = [["1", "one"],
                       ["2", "two"]] }
                                 (Csv.parse "id,value\n1,one\n2,two\n")
-        , test "parse" <|
+        , test "parse quotes" <|
             \() ->
                 Expect.equal
                       { headers = ["id", "value"], records = [["1", "one"],
                       ["2", "two"]] }
                                 (Csv.parse
                                 "\"id\",\"value\"\n\"1\",\"one\"\n\"2\",\"two\"\n")
+        , test "parse quotes on semi-colon" <|
+            \() ->
+                Expect.equal
+                      { headers = ["id", "value"], records = [["1", "one"],
+                      ["2", "two"]] }
+                                (Csv.parseWith ";"
+                                "\"id\";\"value\"\n\"1\";\"one\"\n\"2\";\"two\"\n")
         ]
